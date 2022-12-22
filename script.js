24aecc0f0aea3c64b7c953dfecdde9bd3795e072
Luis Guillen <lag2993@gmail.com>
2022-12-22 00:31:44 -0500

    Tightened up comments and attempted to add rare case catch for randomize error 2nd attempt

--- script.js
+++ script.js
@@ -1,67 +1,86 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Pattern elements
+
 
 // Set Min and Max password lengths
 const PMin = 8;
 var Pmax = 128;
 
 var keyMatch = [];
-var keyCha = [];
-for (let i=32; i<127; i++){
-  keyCha.push(String.fromCharCode(i));
-}
-console.log(keyCha);
 
-// keyCha generation - Array analysis 
-// 0-16, 27-33, 60-65, 91-94 Special Characters - spacing: 16,7,6,8
-// 16 -25 Number - spacing: 10
-// 33-58 Uppercase - spacing: 24
-// 65 - 89 lowercase - spacing: 24
+
+// keyCha generation - Array analysis- from looking thhrough UT-16
+// Special Characters array location 0-16, 27-33, 60-65, 91-94 - spacing: 16,7,6,8
+// Number array location 16 -25 - spacing: 10
+// Uppercase array location  33-58 - spacing: 24
+// lowercase array location 65 - 89  - spacing: 24
  
-// Password Parameters 
-// lowercase
+
 function generatePassword(){
-var lowC = confirm("Would you like the password to contain lowercase characters?");
+  // Password Parameters
+  Pmax = prompt("How many characters should your password be?", "Please choosee between 8 - 128 characters");
+  let outPut;
+  
+  if (Pmax < 8){
+    alert("Too little Characters!")
+    generatePassword();
+  
+  } else if (Pmax > 128){
+    alert("Too many Characters!")
+    generatePassword();
+  } else {; 
 
-
-// uppercase
-var UppC = confirm("Would you like the password to contain uppercase characters?");
+// lowercase
+  var lowC = confirm("Would you like the password to contain lowercase characters?");
+// uppercase 
+  var UppC = confirm("Would you like the password to contain uppercase characters?");
 // numeric
-var NumC = confirm("Would you like the password to contain numbers?");
+  var NumC = confirm("Would you like the password to contain numbers?");
 // special characters
+  var SpcC = confirm("Would you like the password to contain special characters?");
 
-var SpcC = confirm("Would you like the password to contain special characters?");
+// Generating Possible Character Array
+  const keyCha = [];
+  for (let i=32; i<127; i++){
+  keyCha.push(String.fromCharCode(i));
+  };   
+//Copying Character Array to remove elements according to confirms. 
+  var keyUpdt = keyCha;
+  console.log(keyUpdt);
 
-Pmax = prompt("How many characters should your password be?", "Please choosee between 8 - 128 characters");
 
-if (Pmax < 8){
-  alert("Too little Characters!")
-} else if (Pmax > 128){
-  alert("Too many Characters!")
-};
 
-var keyUpdt = keyCha;
+//Generating arrays to check if elements exist in password. 
+  var keyTempt = keyCha;
+  console.log(keyTempt);
+  var lowArr = keyTempt.splice(keyTempt.indexOf("a"),24);
+  console.log(lowArr);
 
-if (lowC === false){
+// Checking Confirms to splice out unwanted elements
+
+  if (lowC === false){
   // indentify where lower case starts which is "a" then splice 26 from there.
   let lowCut = keyUpdt.indexOf("a");
   keyUpdt.splice(lowCut,26);
 
-}
-if (UppC === false){
-    // indentify where Upper case starts which is "A" then splice 26 from there.
+  }
+  else{ 
+
+
+  }
+  if (UppC === false){
+  // indentify where Upper case starts which is "A" then splice 26 from there.
   let UpCut = keyUpdt.indexOf("A");
   keyUpdt.splice(UpCut,26);
-}
-if (NumC === false){
-    // indentify where numbers start which is "0" then splice 10 from there.
+  }
+  if (NumC === false){
+  // indentify where numbers start which is "0" then splice 10 from there.
     let NumCut = keyUpdt.indexOf("0");
     keyUpdt.splice(NumCut,10);
-}
-if (SpcC === false){
-  // indentify where special case groups starts
+  } 
+  if (SpcC === false){
+  // indentify where special case groups starts:
   // " " is the first element then splice 16
   let SpcCut1 = keyUpdt.indexOf(" ");
   keyUpdt.splice(SpcCut1,16);
@@ -73,63 +92,68 @@ if (SpcC === false){
   keyUpdt.splice(SpcCut3,6);
   // "{" is the first element in 4th group then splice 8;
   let SpcCut4 = keyUpdt.indexOf("{");
-  keyUpdt.splice(SpcCut4,8);
-}
-
-  // create reset so that the page will reload after elements are cut out to 
-  // start from scratch.
+  keyUpdt.splice(SpcCut4,8);  
+  }
   // random num key generator;
   let password = [];
   for (let i = 0;  i<Pmax;i++){
-    password[i] = Math.floor(Math.random()*(keyUpdt.length-1)); 
-    password[i] = keyUpdt[password[i]];
+    var location = Math.floor(Math.random()*(keyUpdt.length-1)); 
+    password[i] = keyUpdt[location];
   }
-console.log(password);
-// join password into one output string
-console.log(password.length);
-let outPut = password.join("");
-console.log(outPut);
-console.log(password);
-keyUpdt = keyCha;
+  // password array check
+  console.log(password);
+// join password into one output string 
+  outPut = password.join("");
+  // Reset Array
+  keyUpdt = keyCha;
 
-
-
-return outPut;
-}
+// Conditionals for rare occassion that .random does not choose an element from  the appropriate array. 
+// if(lowC === true){
+//   let lowAChck = lowArr.every( item => {
+//     return outPut.includes(item);
+//   });
+//   if (lowAChck === false){
+//     generatePassword();
+//   };
+// }  
+// if(UppC === true){
+//     let UppAChck = UppArr.every( item => {
+//       return outPut.includes(item);
+//     });
+//     if (UppAChck === false){
+//       generatePassword();
+//     }
+    
+// }
+// if(NumC === true){
+//     let NumAChck = NumArr.every( item => {
+//       return outPut.includes(item);
+//     });
+//     if (NumAChck === false){
+//       generatePassword();
+//     }
+    
+// }
+// if(SpcC === true){
+//     let SpcAChck = SpcC.every( item => {
+//       return outPut.includes(item);
+//     });
+//     if (SpcAChck === false){
+//       generatePassword();
+//     }    
+// }
+  }
+return outPut
+};
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
-
-
-
 }
 
-// Add event listener to generate button
+// // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-// Attempt to add confirm option and copy password to clipboard. 
-// generateBtn.addEventListener("click", confirmButn);
-
-// function confirmButn(){
-
-// let finalReq = confirm("Are you happy with your password?");
-
-// if (finalReq === true){
-// // navigator.clipboard.writeText(outPut).then(
-// //   () => {
-// //     
-// //   }
-
-// // );
-// // alert("The password has been copied.")
-//   }else {
-//   writePassword();
-
-// }
-// }
